a17bd72b2166921f358480b38a4aa410607b4b18
coder prof <you@example.com>
2023-12-30 07:37:46 +0200

    changes

---
+++ app/about/page.jsx
@@ -0,0 +1,12 @@
+import About from "./component";
+import { api } from "@/public/script/public";
+
+export const metadata = {title: 'من نحن'}
+
+export default async function Page () {
+
+    const data = await api('default');
+
+    return <About categories={data.categories || []} settings={data.settings || {}}/>
+        
+}
---
+++ app/contact/component.jsx
@@ -0,0 +1,30 @@
+"use client";
+import Nav from "@/app/component/nav";
+import Footer from "@/app/component/footer";
+import { Fragment } from "react";
+
+export default function About ({ categories, settings }) {
+
+    return (
+
+        <Fragment>
+
+            <Nav categories={categories} settings={settings}/>
+            
+            <div className="container">
+
+                <main style={{ height: "30rem" }} className="flex">
+
+                    تواصل معنا
+
+                </main>
+
+                <Footer pae="تواصل معنا" settings={settings}/>
+
+            </div>
+
+        </Fragment>
+
+    )
+
+}
