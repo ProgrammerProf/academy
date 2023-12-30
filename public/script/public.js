981c44782afaec3ceb0eb5a5df87bc2bdfba9537
coder prof <you@example.com>
2023-12-30 06:48:16 +0200

    changes

--- public/script/public.js
+++ public/script/public.js
@@ -1,5 +1,5 @@
 import CryptoJS from "./crypto";
-export const host = "http://127.0.0.1:8000";
+export const host = "https://homeschool.pythonanywhere.com";
 
 export function sound (src, vol) {
         
